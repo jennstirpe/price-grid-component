9c6000e917185722ad69d576263ca34327b6f0ce
jstirpe <jennstirpe@yahoo.com>
2022-05-04 18:45:28 -0400

    Changed heading size

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -3,7 +3,7 @@ import { StyledSubscription } from "./styled/Subscription.styled"
 const Subscription = () => {
   return (
     <StyledSubscription>
-      <h3 class="subscription-type">Monthly Subscription</h3>
+      <h2 class="subscription-type">Monthly Subscription</h2>
       <p class="pricing"><span class="price">&#36;29</span> per month</p>
       <p class="subscription-desc">Full access for less than &#36;1 a day</p>
       <a href="#" target="_blank" id="signup">Sign Up</a>
